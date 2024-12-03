75b5be03a317d1068b2c507038e3e0316d8e1a38
Den <den@m.com>
2024-12-03 17:18:58 +0300

    add page for lost pages, add registration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MlCheckComponent } from './ml-check/ml-check.component';
 import { InspectionResultsComponent } from './inspection-results/inspection-results.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
+    {path: '', component: MainComponent},
     {path: 'login', component: LoginComponent},
     {path: 'ml_check', component: MlCheckComponent},
-    {path: 'inspection_results', component: InspectionResultsComponent}
+    {path: 'inspection_results', component: InspectionResultsComponent},
+    {path: 'registration', component: RegistrationComponent},
+
+    {path: '**', pathMatch: 'full', component: PageNotFoundComponent}
 ];
