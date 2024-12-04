f711bd34e0774f42684e80870ec7fbaa5271cf68
Den <den@m.com>
2024-12-04 11:44:54 +0300

    create 2 pages in account

--- src/app/user/info/info.component.ts
+++ src/app/user/info/info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-info',
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './info.component.sass'
 })
 export class InfoComponent {
+  @ViewChild('img') img!:  ElementRef<HTMLImageElement>;
+  @ViewChild('file') file!:  ElementRef<HTMLInputElement>;
 
+  changeImage() {
+    // console.info('Image:', this.file.nativeElement.files);
+    if (this.file.nativeElement.files && this.file.nativeElement.files[0]) {
+      let reader = new FileReader()
+
+      reader.onload = (e: any) => {
+        // console.info(e.target.result);
+        this.img.nativeElement.src = e.target.result;
+      };
+
+      // console.info(this.file.nativeElement.files[0]);
+      reader.readAsDataURL(this.file.nativeElement.files[0]);
+
+    }
+
+  }
 }
